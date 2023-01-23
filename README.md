be4bd4f4b211672aa5f3b2c0f11b590f8f869380
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-01-23 21:29:48 +0000

    readme update

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 JS Code Quiz
 
 ## description
+Coding quiz using javascript CSS and HTML. The quiz has a start page and highscores. A timer is used to track th users time and time is deducted or added dependant on answers.
 
 ## screenshot
+![screenshot of question](./assets/screencapture-markmos1991-github-io-code-quiz-2023-01-23-21_25_52.png)
 
 ## link
+https://markmos1991.github.io/code-quiz/
--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -8,9 +8,15 @@ const allQuestions = [
     },
 
     {
-        question: " 2 What is the keyword used to create a function in JavaScript?",
-        choices: ["function", "method", "procedure", "I choose you!!"],
-        answer: "function"
+        question: " What is a boolean?",
+        choices: ["function", "method", "procedure", "number"],
+        answer: "number"
+      },
+
+      {
+        question: " ",
+        choices: ["function", "method", "procedure", "number"],
+        answer: "number"
       },
 
   ];
